59e02d19cac95cc391751edcc5d15d1a51eb3019
Hannah Schriever <hschriever4@gmail.com>
2022-04-13 17:59:17 -0400

    tidying up

--- notebooks/.ipynb_checkpoints/TEST-checkpoint.R
+++
@@ -1,9 +0,0 @@
-# max.R
-# Fetch command line arguments
-# Convert to numerics
-nums = c(5,6,7,8,9,2)
-# cat will write the result to the stdout stream
-cat(max(nums))
-print('hi')
-
-write.table(nums,"test_nums.txt",sep="\t",row.names=FALSE)
